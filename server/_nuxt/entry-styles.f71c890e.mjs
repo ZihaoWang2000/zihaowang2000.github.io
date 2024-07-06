6d3a21d96ddeb57960591669851978385b256230
Zihao Wang <114076648+ZihaoWang2000@users.noreply.github.com>
2024-07-06 15:49:57 +0200

    Delete server directory

--- server/_nuxt/entry-styles.f71c890e.mjs
+++
@@ -1,8 +0,0 @@
-import style_0 from './entry-styles-1.mjs-28436cb1.js';
-import style_1 from './entry-styles-2.mjs-a38182d8.js';
-import style_2 from './entry-styles-3.mjs-8abaefb8.js';
-import style_3 from './entry-styles-4.mjs-4a508c97.js';
-import style_4 from './entry-styles-5.mjs-bcd69811.js';
-import style_5 from './entry-styles-6.mjs-7a361e9f.js';
-import style_6 from './entry-styles-7.mjs-1b045a55.js';
-export default [style_0, style_1, style_2, style_3, style_4, style_5, style_6]
--- server/_nuxt/portfolio-styles.90ee36f0.mjs
+++
@@ -1,3 +0,0 @@
-import style_0 from './portfolio-styles-1.mjs-92d23ae6.js';
-import style_1 from './portfolio-styles-1.mjs-92d23ae6.js';
-export default [style_0, style_1]
