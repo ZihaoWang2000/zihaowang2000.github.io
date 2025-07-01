c4215d11208b55b75da5359fc28fe32a9f3101ce
ZihaoWang2000 <ZihaoWang2000@users.noreply.github.com>
2025-07-01 21:32:22 +0000

    deploy: 72544084f7f98ec96ecb10da4afe1131015572e8

---
+++ _next/static/9rEO_-uuzfR1Lv7lqxsDq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/645-a83155ce63679ae7.js","static/css/4fdb5ce23570bafb.css","static/chunks/pages/index-a7f96be1d7ada6e6.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
